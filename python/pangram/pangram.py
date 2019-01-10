5f47623462c12b1de17373800df64bc504436965
Paulo Campos Zava <camposzava@gmail.com>
2019-01-11 00:26:20 +0100

    pangram

---
+++ python/pangram/pangram.py
@@ -0,0 +1,8 @@
+from string import ascii_lowercase
+
+def is_pangram(sentence):
+    sentence = sentence.lower()
+    for letter in ascii_lowercase:
+        if letter not in sentence:
+            return False
+    return True
