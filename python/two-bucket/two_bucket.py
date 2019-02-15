0c11d36b5d27f0396e2f1937b59cadd5c25321b2
Paulo Campos Zava <camposzava@gmail.com>
2019-02-15 19:13:49 +0100

    two bucket

---
+++ python/two-bucket/two_bucket.py
@@ -0,0 +1,6 @@
+def measure(bucket_one, bucket_two, goal, start_bucket):
+    pass
+
+
+with open('/home/laumzav/PycharmProjects/exerciscm/python/two-bucket/teste.txt', 'a') as f:
+    f.writelines('teste')
